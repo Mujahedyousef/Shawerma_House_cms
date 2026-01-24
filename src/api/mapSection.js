02dddd7f3fe8b2d0b97cf53e289ca023d843f5f1
mohammadmokdad <mohammad.mokdad@joacademy.com>
2026-01-24 08:21:28 +0300

    feat: add Branches Map Manager with theme-aware pin colors

---
+++ src/api/mapSection.js
@@ -0,0 +1,54 @@
+import API from '../services/API';
+
+// Map Section APIs
+export const getActiveMapSection = async () => {
+  const response = await API.get('/map-section/active');
+  return response.data;
+};
+
+export const getMapSectionById = async (id) => {
+  const response = await API.get(`/map-section/${id}`);
+  return response.data;
+};
+
+export const createMapSection = async (data) => {
+  const response = await API.post('/map-section', data);
+  return response.data;
+};
+
+export const updateMapSection = async (id, data) => {
+  const response = await API.put(`/map-section/${id}`, data);
+  return response.data;
+};
+
+export const deleteMapSection = async (id) => {
+  const response = await API.delete(`/map-section/${id}`);
+  return response.data;
+};
+
+// Branch APIs
+export const getBranches = async (mapSectionId) => {
+  const response = await API.get(`/map-section/${mapSectionId}/branches`);
+  return response.data;
+};
+
+export const getBranchById = async (id) => {
+  const response = await API.get(`/map-section/branches/${id}`);
+  return response.data;
+};
+
+export const createBranch = async (data) => {
+  const response = await API.post('/map-section/branches', data);
+  return response.data;
+};
+
+export const updateBranch = async (id, data) => {
+  const response = await API.put(`/map-section/branches/${id}`, data);
+  return response.data;
+};
+
+export const deleteBranch = async (id) => {
+  const response = await API.delete(`/map-section/branches/${id}`);
+  return response.data;
+};
+
